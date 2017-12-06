262cfeefe661a8ce841c5d03b435d7886b5ad0f5
wuguangyao <wuguangyao@lexiangbao.com>
2017-12-06 16:50:08 +0800

    页面返回ftl

--- src/main/java/com/example/demo/hello_world/HelloWorld.java
+++ src/main/java/com/example/demo/hello_world/HelloWorld.java
@@ -1,19 +1,24 @@
 package com.example.demo.hello_world;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class HelloWorld {
 
 
-    @RequestMapping("/hello-world")
+    @RequestMapping("/hello-world-json")
+    @ResponseBody
     String home() {
         System.out.println("hello world !");
         return "Hello World!";
     }
-
+    @RequestMapping("/hello-world-page")
+    String page() {
+        return "demo/hello_world/hello_world";
+    }
 
 }
